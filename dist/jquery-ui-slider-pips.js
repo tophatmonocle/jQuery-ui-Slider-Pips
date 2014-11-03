5e88958c7bdf9852adb15eb9fa7c82aab67b6972
Carlos <carlos@tophatmonocle.com>
2014-11-03 15:47:39 -0500

    Update jquery-ui-slider-pips.js
    Added 'float' changes to dist version

--- dist/jquery-ui-slider-pips.js
+++ dist/jquery-ui-slider-pips.js
@@ -9,7 +9,7 @@
 
     var extensionMethods = {
 
-        pips: function( settings ) {
+        'pips': function( settings ) {
 
             var slider = this,
                 collection = "",
@@ -352,7 +352,7 @@
 
     var extensionMethods = {
 
-        float: function( settings ) {
+        'float': function( settings ) {
 
             var slider = this,
                 $tip,
